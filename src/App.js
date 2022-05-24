e63f8af5923d1f55570640953274665f3b8f5a3c
RobinRattasep <rattasep.robin@khk.ee>
2022-05-24 12:14:36 +0300

    aawd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+require("react/package.json"); // react is a peer dependency.
+var ReactSlider = require("react-slider")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [height, setHeight] = useState(0);
+    const [mass, setMass] = useState(0);
+    const [bmi, setBmi] = useState(0);
+
+    const calculate = (e) => {
+        e.preventDefault();
+        const formValid = +height > 0 && +mass > 0;
+        if (!formValid) {
+            return;
+        }
+        const bmi = +mass / (+height) ** 2;
+        setBmi(bmi);
+    };
+
+
+
+    return (
+        <div className="App">
+            <form onSubmit={calculate}>
+                <div>
+                    <label>height in meters</label>
+                    <input value={height} onChange={(e) => setHeight(e.target.value)} />
+                </div>
+
+                <div>
+                    <label>mass in kg</label>
+                    <input value={mass} onChange={(e) => setMass(e.target.value)} />
+                </div>
+
+                <button type="submit">calculate</button>
+            </form>
+            <ReactSlider
+                className="horizontal-slider"
+                thumbClassName="example-thumb"
+                trackClassName="example-track"
+                renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+            />
+            <p>bmi: {bmi}</p>
+        </div>
+    );
 }
-
-export default App;
